6e55a6b9f3c104c9c09cc0c86cb0abe23315e345
maninzoo <maninzoo@inswave.com>
2015-09-01 19:33:48 +0900

    Call treeWalker() recursively

--- reorder.js
+++ reorder.js
@@ -1,4 +1,5 @@
 var Nightmare = require('nightmare');
+var _ = require('underscore');
 
 // http://yahoo.com
 var testUrl = 'http://127.0.0.1:8080/w5_skt/websquare.html?w2xPath=/w5_samples/ZCAMSTGT00318.xml';
@@ -15,26 +16,55 @@ new Nightmare()
     var comStructure = [],
         body = document.body,
         depth = 0,
+        checkWClass = function( classList, classKeys ) {
+          var j = 0,
+              wClass = '';
+
+          for ( j; j < classKeys.length; j++ ) {
+            if ( classList[classKeys[j]].indexOf('w2') === 0 ) {
+              wClass = classList[classKeys[j]];
+              break;
+            }
+          }
+
+          return wClass;
+        },
+        checkComplexCom = function ( wClass ) {
+          var complexComList = {
+            'w2grid': 'w2grid',
+            'w2selectbox': 'w2selectbox',
+            'w2inputCalendar_div': 'w2inputCalendar_div',
+            'w2radio': 'w2radio'
+          };
+
+          return complexComList[wClass];
+        },
         treeWalker = function ( ele, parentID, comStructure ) {
           var i,
               item,
+              classList,
+              wClass,
               result;
 
           for ( i = 0; i < ele.childElementCount; i++ ) {
-            if ( ele.childNodes[i].id ) {
+            item = ele.childNodes[i];
+            classList = item.classList;
+            wClass = checkWClass( classList, Object.keys(classList) );
+
+            if ( item.id && wClass ) {
               result = {};
-              item = ele.childNodes[i];
               result.id = item.id;
               result.geo = item.getBoundingClientRect();
               result.childCount = item.childElementCount;
               result.name = item.localName;
-              result.classList = item.classList;
+              result.classList = classList;
+              result.wClass = wClass;
 
               if ( item.childElementCount > 0 ) {
-                result.childItems = [];
-
-                if ( depth < 2 ) {
+                //if ( depth < 2 ) {
+                if ( !checkComplexCom(wClass) ) {
                   depth += 1;
+                  result.childItems = [];
                   treeWalker( item, result.id, result.childItems );
                   depth -= 1;
                 }
