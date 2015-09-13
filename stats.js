eb93d22a7ca487850b47d0783627fd42e2b2ad9a
maninzoo <maninzoo@inswave.com>
2015-09-13 21:14:10 +0900

    Basic work

--- stats.js
+++ stats.js
@@ -1,10 +1,81 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs'),
+    path = require('path');
+
+var domParser = require('xmldom').DOMParser,
+    xpath = require('xpath');
 
 var ArrayProto = Array.prototype,
     push       = ArrayProto.push;
 
-var targetDir = '/Users/maninzoo/Contents/temp/09_13_2015/mp/xml/';
+var targetDir = '/Users/maninzoo/Contents/webstorm/projects/websquare-refiner/samples/';
+//var targetDir = '/Users/maninzoo/Contents/temp/09_13_2015/mp/xml/';
+
+var storeData = function ( node, result ) {
+  var i,
+      nodeName,
+      nodes,
+      namedNodeMap,
+      attr;
+
+  // ELEMENT_NODE
+  if ( node.nodeType === 1 ) {
+    nodeName = node.nodeName;
+
+    if ( result[nodeName] ) {
+      result[nodeName].count += 1;
+    } else {
+      console.log( nodeName );
+      result[nodeName] = {};
+      result[nodeName].count = 1;
+    }
+
+    if ( node.hasAttributes() ) {
+      if ( !result[nodeName].attrs ) {
+        result[nodeName].attrs = {};
+      }
+      namedNodeMap = node.attributes;
+
+      for ( i = 0; i < namedNodeMap.length; i++ ) {
+        attr = namedNodeMap.item(i);
+
+        if ( result[nodeName].attrs[attr.name] ) {
+          result[nodeName].attrs[attr.name] += 1;
+        } else {
+          result[nodeName].attrs[attr.name] = 1;
+        }
+      }
+    }
+
+    if ( nodeName !== 'Grid' ) {
+      if ( node.hasChildNodes() ) {
+        nodes = node.childNodes;
+        for ( i = 0; i < nodes.length; i++ ) {
+          storeData( nodes[i], result );
+        }
+      }
+    }
+  }
+};
+
+var collectUsage = function(list) {
+  var result = {};
+
+  list.forEach( function( file, i ) {
+    //console.log( i + ' ' + file );
+
+    var content = fs.readFileSync( file, 'utf-8' ),
+        doc = new domParser().parseFromString(content),
+        nodes = doc.childNodes;
+
+    var i;
+
+    for ( i = 0; i < nodes.length; i++ ) {
+      storeData( nodes[i], result );
+    }
+  } );
+
+  console.log( JSON.stringify(result) );
+};
 
 var getFileList = function( dir, done ) {
   var results = [];
@@ -32,12 +103,6 @@ var getFileList = function( dir, done ) {
         }
       } );
     } );
-  } );
-};
-
-var collectUsage = function(list) {
-  list.forEach( function( file, i ) {
-    console.log( i + ' ' + file );
   } );
 };
 
