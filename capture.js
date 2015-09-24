5db2daafea594fb84212c5f9b7714314d687aea6
maninzoo <maninzoo@inswave.com>
2015-09-24 16:45:01 +0900

    Add capture func

--- capture.js
+++ capture.js
@@ -1,14 +1,21 @@
 var Nightmare = require('nightmare'),
-    fileManager = require('./util/fileManager');
+    fs = require('fs'),
+    path = require('path');
 
-var size = {
-  general: {
-    width: 1440, height: 910
-  },
-  mobile: {
-    width: 1044, height: 828
-  }
-};
+//var fileManager = require('./util/fileManager');
+
+var confFile = '/Users/maninzoo/Contents/temp/09_24_2015/capture/capture.conf.json',
+    size = {
+      general: {
+        width: 1440, height: 910
+      },
+      mobile: {
+        width: 1044, height: 828
+      }
+    },
+    waitCondition = {
+
+    };
 /*
 //var testUrl = 'http://swing.websquare.co.kr/websquare/websquare.html?w2xPath=/SWING/sample/main_fv.xml';
 var testUrl = 'http://swing.websquare.co.kr/websquare/websquare.html?w2xPath=/SWING/conversion/ZORDSB0900010_WS5.xml';
@@ -28,6 +35,34 @@ new Nightmare()
   } );
 */
 
-fileManager.getFileList( '/Users/maninzoo/Contents/temp/09_24_2015/SWING/', function( err, list ) {
-  console.log( JSON.stringify(list) );
-} );
+//fileManager.getFileList( '/Users/maninzoo/Contents/temp/09_24_2015/SWING/', function( err, list ) {
+//  console.log( JSON.stringify(list) );
+//} );
+
+var content = fs.readFileSync( confFile, 'utf-8' ),
+    conf = JSON.parse( content ),
+    general = conf.w2xPath.general,
+    mobile = conf.w2xPath.mobile;
+
+var capture = function capture ( list, type ) {
+  for ( p in list ) {
+    var w2xPath = list[p],
+        url = conf.baseURL + '?w2xPath=' + w2xPath,
+        nightmare = new Nightmare();
+
+    console.log(url);
+
+    nightmare
+      .viewport( size[type].width, size[type].height )
+      .goto( url )
+      .wait( function() {
+        return $W ? $W.scriptSectionExcuteFlag : '';
+      }, 'executed' )
+      .screenshot( path.resolve( conf.basePath + w2xPath.substring( 0, w2xPath.length - 3 ) + 'png' ) )
+      .run( function( err, nightmare ) {
+        console.log( 'Done.' );
+      } );
+  }
+};
+
+capture( general, 'general' );
